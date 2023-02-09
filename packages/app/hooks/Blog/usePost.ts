cf037bba29ed4bf77813c6662789d52c6c7ea529
Niyaz Jamal <niyaz.jm@icarasia.com>
2023-02-09 10:25:56 +0800

    Commit with fix expo build issues with mono repo

--- packages/app/hooks/Blog/usePost.ts
+++ packages/app/hooks/Blog/usePost.ts
@@ -1,6 +1,8 @@
 import useSWR from 'swr'
 import { fetcher } from 'app/lib/fetcher'
 
+/** @todo - move base urls to env later */
+
 export const usePost = (id: String) => {
   const { data, error, isLoading } = useSWR(
     `https://mockend.com/mockend/demo/posts/${id}`,
